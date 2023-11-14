8ebf85a72ab61f2b65c4d4387ecfb8e1823612e8
juan galeano <jgaleano@quadiontech.com>
2023-11-13 22:34:01 -0300

    pre entrega 2 objetos array

--- scripts.js
+++ scripts.js
@@ -10,31 +10,67 @@ function menu(){
     }while(option == 0);
     return option;
 }
-//Se obtiene el valor del producto dependiendo de la opcion
-function selectorProducto(option){
-    let valor=0;
-    switch(option){
-        case 1:
-            valor = parseFloat(prompt("Ingrese el valor aproximado de su auto"))
-            break;
-        case 2:
-            valor = parseFloat(prompt("Ingrese el valor aproximado de su moto"))
-            break;
-        case 3:
-            valor = parseFloat(prompt("Ingrese el valor aproximado de su notebook"))
-            break;
-        case 4:
-            valor = parseFloat(prompt("Ingrese el valor aproximado de su casa"))
-            break;
-    }
-    return valor;
-}
 
 //se calcula el valor de la cuota calculando el 10% del valor total
-function calculadorCuotas(valor){
-    
-    let cuota = parseFloat(valor*0.10)    
-    alert("El valor de su cuota mensual es de: "+cuota);
+function calculadorCuotas(valor){    
+    let cuota = parseFloat(valor*0.10) 
+    return cuota;       
 }
-//Llamada a las funciones
-calculadorCuotas(selectorProducto(menu()))
+//Se obtiene el valor del producto dependiendo de la opcion
+function selectorProducto(option){
+    let cuotas = [];
+    switch(option){
+        case 1:      
+            const car = {marca:"auto", valor:0, cuota:0.0};      
+            let marca = prompt("Ingrese la marca de su auto")
+            valor = parseFloat(prompt("Ingrese el valor aproximado de su auto"))
+            car.marca = marca
+            car.valor = valor
+            car.cuota = calculadorCuotas(valor)
+            console.log("El valor de la cuota es: ",  car.cuota)
+            cuotas.push(car.cuota)
+            break;                      
+        case 2:
+            const moto = {tipo:"moto", valor:0, cuota:0.0};      
+            let tipo = prompt("Ingrese la marca de su moto")
+            valor = parseFloat(prompt("Ingrese el valor aproximado de su moto"))
+            moto.tipo = tipo
+            moto.valor = valor 
+            moto.cuota = calculadorCuotas(valor)    
+            console.log("El valor de la cuota es: ",  moto.cuota)
+            cuotas.push(moto.cuota)
+            break;
+        case 3:
+            const notebook = {modelo:"moto", valor:0, cuota:0.0};      
+            let modelo = prompt("Ingrese la marca de su notebook")
+            valor = parseFloat(prompt("Ingrese el valor aproximado de su notebook"))
+            notebook.modelo = modelo
+            notebook.valor = valor   
+            notebook.cuota = calculadorCuotas(valor) 
+            console.log("El valor de la cuota es: ",  notebook.cuota)   
+            cuotas.push(notebook.cuota)
+            break;
+        case 4:
+            const casa = {direccion:"direccion", valor:0, cuota:0.0};      
+            let direccion = prompt("Ingrese su direccion")
+            valor = parseFloat(prompt("Ingrese el valor aproximado de su casa"))
+            casa.direccion = direccion
+            casa.valor = valor 
+            casa.cuota = calculadorCuotas(valor)
+            console.log("El valor de la cuota es: ",  casa.cuota)
+            cuotas.push(casa.cuota)
+            break;
+    }   
+    return cuotas
+}
+
+let mis_cuotas = selectorProducto(menu());
+let total = 0
+for(let i=0; i < mis_cuotas.length ; i++){
+    total = total + parseInt(mis_cuotas[i])
+}
+console.log("La suma de tus cuotas es: ", total)
+
+
+
+
